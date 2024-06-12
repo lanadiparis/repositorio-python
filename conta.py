65c5ca2da8527e09c3b9fae04f16b9c834e34c59
lana ★ <alannabrenda77@gmail.com>
2024-06-12 14:22:00 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,6 @@
+salario_base = 1765.43  
+aumento_percentual = 5  
+aumento = salario_base * (aumento_percentual / 100)
+novo_salario = salario_base + aumento
+
+print("O aumento de 5% sobre o salário base de R$ 1765,43 é: R$", round(aumento, 2))
